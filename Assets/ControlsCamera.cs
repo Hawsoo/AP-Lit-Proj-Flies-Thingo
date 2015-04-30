f1a20c428eeabc1c4e7fda30366bf16752f1db2b
Hawsoo <timoguy99004@gmail.com>
2015-04-30 06:23:27 -0700

    Creates camera panning system

---
+++ Assets/ControlsCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsCamera : MonoBehaviour
+{
+    public bool skipZoomOut = false;
+
+    private bool keepZooming = true;
+    private float origZoom;
+    private float targetZoom = 1;
+    private float zoomFactor = 2;
+
+    // Init
+    void Start()
+    {
+        // Init
+        origZoom = transform.localScale.x - 0.05f;      // Assumed local scale is same coords
+
+        // Completely zoom out if wanted to skip
+        if (skipZoomOut)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            keepZooming = false;
+        }
+    }
+
+    // Update
+    void Update()
+    {
+        // Zoom out if needed
+        if (keepZooming)
+        {
+            // Calculate delta zoom
+            float deltaFromOrig = (transform.localScale.x - origZoom) * zoomFactor;
+            float deltaToTarget = (targetZoom - transform.localScale.x) * zoomFactor;
+            float delta = Mathf.Min(deltaFromOrig, deltaToTarget);
+            
+            // Apply zoom
+            transform.localScale += new Vector3(delta, delta, delta) * Time.deltaTime;
+        }
+    }
+}
